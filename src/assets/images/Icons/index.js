2194b03e57759d53db34f7956815c044a53a07e5
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-08-10 14:56:21 -0300

    build: added index.js to help with assets imports

---
+++ src/assets/images/Icons/index.js
@@ -0,0 +1,21 @@
+import programmingIcon from "./programmingIcon.svg";
+import marketingIcon from "./marketingIcon.svg";
+import architectureIcon from "./architectureIcon.svg";
+import financialIcon from "./financialIcon.svg";
+import handshakeIcon from "./handshakeIcon.svg";
+import healthIcon from "./healthIcon.svg";
+import carIcon from "./carIcon.svg";
+import houseIcon from "./houseIcon.svg";
+import peopleIcon from "./peopleIcon.svg";
+
+export {
+    programmingIcon,
+    marketingIcon,
+    architectureIcon,
+    financialIcon,
+    handshakeIcon,
+    healthIcon,
+    carIcon,
+    houseIcon,
+    peopleIcon
+}
