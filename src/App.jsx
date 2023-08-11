c2a67962d19f61a79adadbb811f38b4b15e8d163
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-08-11 01:49:00 -0300

    feat: created Auth Context & useAuth hook

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import {
   Service
 } from "./pages";
 
+import { AuthProvider } from "./contexts/authContext";
+
 import Header from "./components/Header";
 
 import "./assets/styles/reset.css";
@@ -20,14 +22,16 @@ export default function App() {
 
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/signin" element={<Login />}></Route>
-        <Route path="/signup" element={<Register />}></Route>
-        <Route path="/service" element={<Service />}></Route>
-        <Route path="/service/:role" element={<Service />}></Route>
-      </Routes>
+      <AuthProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signin" element={<Login />}></Route>
+          <Route path="/signup" element={<Register />}></Route>
+          <Route path="/service" element={<Service />}></Route>
+          <Route path="/service/:role" element={<Service />}></Route>
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
