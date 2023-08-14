83b5922a8cf0f699ff172a02378978175ec522cf
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-08-14 13:31:49 -0300

    chore: added VITE API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =  `${import.meta.env.VITE_API_URL}`;
+const BASE_URL =  `${import.meta.env.VITE_API_URL}`
 
 const API = axios.create({
     baseURL: BASE_URL
